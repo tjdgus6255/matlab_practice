01b6cb339c0d4184247e5e72bf54aa0a0c21febf
tjdgus6255 <67773599+tjdgus6255@users.noreply.github.com>
2021-03-18 15:30:16 +0900

    Surf Nasa_2

---
+++ exam_surf_5.m
@@ -0,0 +1,53 @@
+
+clear all; close all; clc;
+
+% global temperature surf plot
+
+data = load('amaps.txt');
+
+log = data(:, 3); % 경도
+lat = data(:, 4); % 위도
+T = data(:, 5); % 온도
+
+index = find(T > 10); %온도가 이상한 값을 찾아냄
+T(index) = NaN; % 온도가 10도가 넘는 값들을 무효화(제거)
+% 3차원 그래프를 그리기 위해서는 matrix를 사용해야 함
+% griddata 사용
+
+[X, Y] = meshgrid(-180:1:180, -90:1:90);
+Temp = griddata(log, lat, T, X, Y) % X, Y는 data mapping을 위해 사용됨
+
+surf(X, Y, Temp)
+view(2)
+shading flat
+colorbar
+colormap hot
+colormap(flipud(colormap))
+caxis([-0.2 4.1]) % 시작점과 끝점 설정
+
+%load image
+img = imread('world_map.png');
+figure(2)
+image([-180 180], [-90 90], flipud(img)) 
+set(gca, 'YDir', 'normal')
+
+hold on
+% image를 불러올때 x범위와 y범위 설정 가능
+% flipud(image)를 사용하여 image 자체를 돌려버림
+Tempfind(isnan(Temp)) = 100;% nan(not a number) 0이 아닌 것을 찾음
+
+surf(X, Y, Temp)
+view(2)
+shading flat
+colorbar
+colormap hot
+colormap(flipud(colormap))
+caxis([-0.2 4.1])
+alpha(0.8)
+ylabel('Latitude [degrees]')
+xlabel('Longitude [degrees]')
+title(['Global Temperature Change ', ...
+      'From 1957 To 2017 ', ...
+      'Source : NASA'])
+  plot([-180 180], [0 0], '-k')
+  plot([0 0], [-90 90], '-k')
